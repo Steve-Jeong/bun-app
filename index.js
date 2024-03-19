253bd5d4d65b6dc652bfa755943def19288c8ec6
Steve Jeong <jst0930@gmail.com>
2024-03-19 10:41:29 +0300

    index.js changed for test if the source fild need to be pulled in the server

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
 const chalk = require("chalk");
 const express = require("express");
 const app = express();
-exports.app = app
+exports.app = app;
 const postRouter = require("./routers/postRouter.js");
 const authRouter = require("./routers/authRouter");
 
 // mongodb setup
-const connectWithRetry = require('./mongo.js')
+const connectWithRetry = require("./mongo.js");
 connectWithRetry();
 
-console.log(chalk.inverse.red('index.js is run again'))
-console.log('app app app : ', app)
+console.log(chalk.inverse.red("index.js is run again"));
+console.log("app app app : ", app);
 
 // redis setup with express-session
-const {redisSetup, redisStore} = require('./redis')
+const { redisSetup, redisStore } = require("./redis");
 redisSetup();
 
-
-app.enable('trust proxy')
-const os=require('os')
+app.enable("trust proxy");
+const os = require("os");
 // app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send('Home page');
+  res.send("Home page");
 });
 app.get("/api/v1", (req, res) => {
   console.log(chalk.inverse.cyan(`${os.hostname()} : Hello world`));
-  res.send(`${os.hostname()} : Hello world`);
+  res.send(`${os.hostname()} : Hello world!!!!123`);
 });
-app.use('/api/v1/post', postRouter)
+app.get("/api/v1/hello", (req, res) => {
+  console.log(chalk.inverse.cyan(`${os.hostname()} : Hello world`));
+  res.send(`${os.hostname()} : Hello world!!!!123`);
+});
+
+app.use("/api/v1/post", postRouter);
 app.use("/api/v1/auth", authRouter);
 
 const PORT = process.env.PORT || 3000;
